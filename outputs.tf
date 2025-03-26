d7abf01b90a3b046470454d9bd3e709c5c68faf8
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-03-26 11:48:04 -0400

    initial check in

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "var_of_random_type" {
+    description = "var_of_random_type's output value"
+    value       = var.var_of_random_type
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "var_of_random_type" {
+    type        = string
+    description = "A variable that may be assigned various custom config types to test the output value."
+}
